098f062845b8ba2acdf9b10d3131a080280ba615
Julian Alarcon <juaalarconma@gmail.com>
2023-03-20 18:54:26 -0500

    wip:base | add bemit css base folders and tailwind imports

---
+++ src/app/App.tsx
@@ -0,0 +1,15 @@
+import Test from './components/Test';
+
+function App() {
+  return (
+    <div className="flex flex-col justify-center">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Test />
+    </div>
+
+  )
+}
+
+export default App
---
+++ src/app/components/Test.jsx
@@ -0,0 +1,13 @@
+import styles from './Test.module.scss';
+
+function Test() {
+  const ctyles = styles['c-test'];
+  return (
+    <div className={`${ctyles} flex items-center bg-white`}>
+      <span>This is just a message</span>
+      <button type="button" className="ml-6 p-12 bg-gray text-white">click me!</button>
+    </div>
+  )
+}
+
+export default Test;

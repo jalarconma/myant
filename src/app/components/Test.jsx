dcfc5b17980d241a16c0fd2515c0c339824d4e8f
Julian Alarcon <juaalarconma@gmail.com>
2023-03-20 19:19:15 -0500

    wip:base | add auto imports to scss files

--- src/app/components/Test.jsx
+++ src/app/components/Test.jsx
@@ -3,7 +3,7 @@ import styles from './Test.module.scss';
 function Test() {
   const ctyles = styles['c-test'];
   return (
-    <div className={`${ctyles} flex items-center bg-white`}>
+    <div className={`${ctyles} flex items-center`}>
       <span>This is just a message</span>
       <button type="button" className="ml-6 p-12 bg-gray text-white">click me!</button>
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: { 
+         // dont need include file extend .scss
+         additionalData: `
+         @import "./src/styles/00.settings/all-settings";
+         @import "./src/styles/01.tools/all-tools";
+         @import "./src/styles/03.components/all-components";
+         @import "./src/styles/04.utilities/all-utilities";
+         ` 
+     },
+    },
+  },
 })

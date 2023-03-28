d6ff1992273a9727591f27c11858b6eb1105685c
Julian Alarcon <juaalarconma@gmail.com>
2023-03-28 15:45:05 -0500

    wip:auth | add amplify package and initial amplify config

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import ReactDOM from 'react-dom/client'
+import { Amplify } from 'aws-amplify';
+
 import App from './app/App'
+import updatedAwsConfig from './configs/amplify-config';
+
 import './index.scss'
+
+Amplify.configure(updatedAwsConfig);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <App />

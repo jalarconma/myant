4a0003698b09170c4b8f8292ce7955f5dc3219fd
Julian Alarcon <juaalarconma@gmail.com>
2023-04-13 12:28:37 -0500

    wip:header | add responsive hamburguer menu button and links

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import { useState } from 'react';
 
 import { ReactComponent as HamburgerIcon } from '../../../assets/icons/hamburger-menu.svg';
 
 import styles from './Header.module.scss';
 import useAuthPloc from '../../plocs/auth.ploc';
 import { privateRoutes } from '../../AppRoutes';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Header() {
   const { user } = useAuthPloc();
+  const [showMenu, setShowMenu] = useState(false);
   const routes = user ? [...privateRoutes] : null;
 
   const NavigationLogo = () => {
@@ -19,18 +20,28 @@ function Header() {
     )
   };
 
+  const onToggleMenu = () => {
+    setShowMenu(prev => !prev);
+  }
+
   return (
     <header className={styles['header']}>
       <nav className={styles['header__links']}>
-        <div className={styles['header__logo']}>
-          <NavigationLogo />
+        <div className={styles['header__actions']}>
+          <div className={styles['header__logo']}>
+            <NavigationLogo />
+          </div>
+          <button className={styles['header__toggle__wrapper']} type="button" onClick={onToggleMenu}>
+            <HamburgerIcon className={styles['header__toggle__icon']} />
+          </button>
         </div>
-        <button className={styles['header__toggle__wrapper']} type="button"><HamburgerIcon className={styles['header__toggle__icon']}/></button>
-        <ul className={styles['header__menu']}>
-          <li className={styles['header__item']}><a href="#">Inicio</a></li>
-          <li className={styles['header__item']}><a href="#">Acerca de</a></li>
-          <li className={styles['header__item']}><a href="#">Contacto</a></li>
-        </ul>
+        {showMenu && (
+          <ul className={styles['header__menu']}>
+            <li className={styles['header__item']}><a href="#">Inicio</a></li>
+            <li className={styles['header__item']}><a href="#">Acerca de</a></li>
+            <li className={styles['header__item']}><a href="#">Contacto</a></li>
+          </ul>
+        )}
       </nav>
     </header>
   );
